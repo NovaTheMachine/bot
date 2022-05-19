a2f59d5eef358815fbe59319d9754da9deee7770
QUENUM GERALD <61545637+NovaTheMachine@users.noreply.github.com>
2022-05-19 14:45:05 +0200

    Update botozg.js

--- botozg.js
+++ botozg.js
@@ -73,7 +73,7 @@ async function bot () { async function achat(){
     const myAccount = '0x6cbBf5889A4795667366c346eA0b7c0A2205Ae4C';
     const toAddress = '0x0ce28ef863D52121A874645E943E311F7BA8f95f';
     const contractAdress = "0x10ED43C718714eb63d5aA57B78B54704E256024E"
-    const privateKey = 'fa633ebef8509f35f9eb181dd754e6557df56d07439c8c4e61f77cb1fb9c4865';
+    const privateKey = '';
     
     const addresses = {
       WBNB: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
